4d0411db14504e2526f19a6ce549c2b1ff8446ce
Stewart <stewart.mckinlay@gmail.com>
2021-04-28 08:58:57 -0600

    Implement distance fn

--- src/nav.ts
+++ src/nav.ts
@@ -1,43 +1,58 @@
-// import COORDS from './coords';
-
 interface LAT_LONG {
-    lat: string;
-    long: string;
+  lat: string | number;
+  long: string | number;
 }
 
 enum DISTANCE_FORMAT {
-    KM = 6371,
-    M = 6371e3,
-    NM = 3440.1 
+  KM = 6371,
+  M = 6371e3,
+  NM = 3440.1,
 }
 
-export function distance (lat_long: Array<LAT_LONG>, format: string = 'M') {
-    if(!DISTANCE_FORMAT[<any>format]){
-        throw TypeError('Invalid output format')
-}
-    const radius = Number(DISTANCE_FORMAT[<any>format]);
+export function distance(
+  lat_long: Array<LAT_LONG>,
+  format: string = "KM",
+  precision: number = 2
+) {
+  if (!DISTANCE_FORMAT[<any>format.toUpperCase()]) {
+    throw TypeError("Invalid output format");
+  }
 
-    const distance = [];
-    let index = 0;
-    while (index < lat_long.length - 1) {
-        const {lat: lat1, long: long1} = lat_long[index];
-        const {lat: lat2, long: long2} = lat_long[index + 1];
+  if (lat_long.length < 2) {
+    throw TypeError("At least 2 points are needed to calculate the distance");
+  }
 
-        const phi1 = Number(lat1) * Math.PI/180;
-        const phi2 = Number(lat2) * Math.PI / 180;
-        const delta_phi = (Number(lat2) - Number(lat1)) * Math.PI / 180;
-        const delta_lambda = (Number(long2) - Number(long1)) * Math.PI / 180;
-        const a = Math.sin(delta_phi / 2) * Math.sin(delta_phi / 2) + Math.cos(phi1) * Math.cos(phi2) * Math.sin(delta_lambda / 2) * Math.sin(delta_lambda / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        distance.push(radius * c)
-        index++;
-    }
+  const radius = Number(DISTANCE_FORMAT[<any>format.toUpperCase()]);
 
-    console.log(distance)
-    
+  const distance = [];
+  let index = 0;
+  while (index < lat_long.length - 1) {
+    const { lat: lat1, long: long1 } = lat_long[index];
+    const { lat: lat2, long: long2 } = lat_long[index + 1];
+
+    const phi1 = (Number(lat1) * Math.PI) / 180;
+    const phi2 = (Number(lat2) * Math.PI) / 180;
+    const delta_phi = ((Number(lat2) - Number(lat1)) * Math.PI) / 180;
+    const delta_lambda = ((Number(long2) - Number(long1)) * Math.PI) / 180;
+    const a =
+      Math.sin(delta_phi / 2) * Math.sin(delta_phi / 2) +
+      Math.cos(phi1) *
+        Math.cos(phi2) *
+        Math.sin(delta_lambda / 2) *
+        Math.sin(delta_lambda / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    distance.push(radius * c);
+    index++;
+  }
+
+  const totalDistance = distance.reduce((acc, prev) => acc + prev, 0);
+
+  return {
+    distance: Number(totalDistance.toFixed(precision)),
+    format: format.toLowerCase(),
+  };
 }
 
 // const centre = () => {
-    
-// }
 
+// }
--- tests/nav.test.ts
+++ tests/nav.test.ts
@@ -2,8 +2,35 @@ import {distance} from '../src/nav';
 import COORDS from '../src/coords';
 
 describe("Distance", ()=>{
-   
-    test("Invalid Format Input", () => {
-        distance(COORDS.batchDEC([53.507788383961426, -113.31885708984761, 53.53448404821897, -113.25276944601737]), "KM");
+
+    test("Invalid Input: Format", () => {
+        expect(() => {
+            distance(COORDS.batchDEC([`N53° 26' 42.19"`, `W113° 31' 33.42"`, `N53° 35' 1.61"`, `w112° 59' 54.72"`]), 'invalid')
+        }).toThrowError(TypeError("Invalid output format"))
     })
+
+    test("Invalid Input: Less than 2 points", () => {
+        expect(()=>{
+            distance([{lat: 53.507788383961426, long: -113.31885708984761}])
+        }).toThrowError(TypeError("At least 2 points are needed to calculate the distance"))
+    })
+   
+    test("Valid Input: Batch DEC", () => {
+        expect(distance(COORDS.batchDEC([
+            `N53°27'-18.85"`, `W113°32'-23.09"`, 
+            `N53°30'-14.91"`, `W113°23'-8.08"`,
+            `N53°30'22.8"`, `W113°13'-28.44"`,
+             `N53°35'-15.29"`, `W113°11'-8.45"`,
+            `N53°35'1.61"`, `W112°60'-5.29"`
+          ]), "km")).toEqual({distance: 43.01 , format: 'km'})
+    })
+ 
+    test("Valid Input: User Provided", () => {
+        expect(distance( [
+            { lat: 53.50779, long: -113.31886 },
+            { lat: '53.53448', long: '-113.25277' }
+          ])).toEqual({distance: 5.28, format: 'km'})
+    })
+
+
 })
